767f6c6268d460c808afac0f369e4af587a716a5
sumomoneko <sumomoneko@gmail.com>
2022-11-07 17:56:27 +0100

    Initial Commit

---
+++ Schriftsetzer/SchriftsetzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchriftsetzerApp.swift
+//  Schriftsetzer
+//
+//  Created by sumomoneko on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SchriftsetzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
